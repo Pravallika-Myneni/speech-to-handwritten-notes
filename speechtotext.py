ee06d75028a83cd0895848f8a3d760c89267c5e3
Pravallika-Myneni <47894231+Pravallika-Myneni@users.noreply.github.com>
2021-04-25 09:07:40 +0530

    text to handwriting

--- speechtotext.py
+++ speechtotext.py
@@ -1,30 +1,23 @@
 #import library
 import speech_recognition as sr
+from keys import GOOGLE_CLOUD_API_KEY
 
-user_choice = input("Enter speech :")
-if(user_choice == "microphone"):
-
+print(GOOGLE_CLOUD_API_KEY)
+def somethingwecopiedonline(file_name):
     r = sr.Recognizer()
 
-    with sr.Microphone() as source:
-        print("Talk")
-        audio_text = r.listen(source)
-        print("Time over, thanks")
-    # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
-        
-        try:
-            # using google speech recognition
-            print("Text: "+r.recognize_google(audio_text))
-        except:
-            print("Sorry, I did not get that")
-elif(user_choice == "recorded"):
-    r = sr.Recognizer()
+    with sr.AudioFile(file_name) as source:
+        audio = r.listen(source)
 
-    with sr.AudioFile('audio.wav') as source:
-        audio_text = r.listen(source)
-        try:
-            text = r.recognize_google(audio_text)
-            print('Converting audio transcripts into text ...')
-            print(text)
-        except:
-            print('Sorry.. run again...')
+    try:
+        # print("Google Cloud Speech thinks you said " + r.recognize_google(audio))
+        result = r.recognize_google(audio)
+        return result
+    except sr.UnknownValueError:
+        print("Google Cloud Speech could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Cloud Speech service; {0}".format(e))
+
+
+def somethingelse():
+    return 23
